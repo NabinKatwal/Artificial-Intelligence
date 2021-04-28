4adc826a69bd56dbad54369519eabfc40b1d5d92
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-04-28 10:29:51 +0545

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -6,26 +6,19 @@ class Hangman:
 
     def get_word(self):
         word_list = ['Cat','Luffy','Artificial']
-        return word_list
+        return random.choice(word_list)
 
-    def difficulty(self, choice):
-        word = self.get_word()
-        if choice.lower() == 'easy' and len(word) < 4:
-            return random.choice(word)
-        elif choice.lower() == 'medium' and 4 <= len(word) <= 8:
-            return random.choice(word)
-        else:
-            return random.choice(word)
+    def difficulty(self):
+        pass
 
     def logic(self, choice):
-        word = self.difficulty(choice)
+        word = self.get_word()
         letter_list = [word[i] for i in range(0, len(word))]
         guess_list = ['_' for i in range(0,len(word))]
         print(word)
 
     def play(self):
-        choice = input("Select difficulty level: 1. Easy 2. Medium 3. Hard ")
-        self.logic(choice)
+        pass 
 
 
 
