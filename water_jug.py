1b8c1b1013762eda4e511baee0d7e3248cace16c
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-08-12 16:03:29 +0545

    Update water_jug.py

--- water_jug.py
+++ water_jug.py
@@ -1,69 +1,44 @@
 from collections import deque
 
 def BFS(a, b, target):
-     
-    # Map is used to store the states, every
-    # state is hashed to binary value to
-    # indicate either that state is visited
-    # before or not
     m = {}
     isSolvable = False
     path = []
-     
-    # Queue to maintain states
     q = deque()
-     
-    # Initialing with initial state
     q.append((0, 0))
  
     while (len(q) > 0):
-         
-        # Current state
         u = q.popleft()
- 
-        #q.pop() #pop off used state
- 
-        # If this state is already visited
+
         if ((u[0], u[1]) in m):
             continue
- 
-        # Doesn't met jug constraints
+
         if ((u[0] > a or u[1] > b or
              u[0] < 0 or u[1] < 0)):
             continue
- 
-        # Filling the vector for constructing
-        # the solution path
+
         path.append([u[0], u[1]])
- 
-        # Marking current state as visited
+
         m[(u[0], u[1])] = 1
- 
-        # If we reach solution state, put ans=1
-        if (u[0] == target or u[1] == target):
+
+        if u[0] == target or u[1] == target:
             isSolvable = True
              
             if (u[0] == target):
                 if (u[1] != 0):
-                     
-                    # Fill final state
+
                     path.append([u[0], 0])
             else:
                 if (u[0] != 0):
- 
-                    # Fill final state
+
                     path.append([0, u[1]])
- 
-            # Print the solution path
+
             sz = len(path)
             for i in range(sz):
                 print("(", path[i][0], ",",
                            path[i][1], ")")
             break
- 
-        # If we have not reached final state
-        # then, start developing intermediate
-        # states to reach solution state
+
         q.append([u[0], b]) # Fill Jug2
         q.append([a, u[1]]) # Fill Jug1
  
@@ -84,14 +59,10 @@ def BFS(a, b, target):
             # Check if this state is possible or not
             if ((c == 0 and c >= 0) or d == b):
                 q.append([c, d])
-         
-        # Empty Jug2
+
         q.append([a, 0])
-         
-        # Empty Jug1
         q.append([0, b])
- 
-    # No, solution exists if ans=0
+
     if (not isSolvable):
         print ("No solution")
 
